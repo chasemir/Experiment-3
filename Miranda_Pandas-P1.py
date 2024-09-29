923ad1a8da61814a7b08a97708e7d5d57b50d12e
chasemir <chasemirr@gmail.com>
2024-09-29 13:22:55 +0800

    Miranda_Pandas-P1.py

--- Miranda_Pandas-P1.py
+++ Miranda_Pandas-P1.py
@@ -3,26 +3,26 @@
 
 # In[1]:
 
-
+# Import pandas
 import pandas as pd
 
 
 # In[2]:
 
-
+# import csv file to the program
 cars = pd.read_csv('cars.csv')
 cars
 
 
 # In[3]:
 
-
+# Display the first five rows
 cars.head(5)
 
 
 # In[4]:
 
-
+# Display the last five rows
 cars.tail(5)
 
 
